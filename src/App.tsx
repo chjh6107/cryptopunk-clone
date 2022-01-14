50bb048003fd9892f8c55d73bda005773c8426f7
chjh6107 <chjh6107@gmail.com>
2022-01-14 23:28:37 +0900

    header 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components';
 import Header from './components/Header'
-
 //https://youtu.be/hhZtiytNaBQ
-function App() {
+//header 49:27까지
+
+const GlobalStyle = styled.div`
+    background-color: #000;
+    padding: 20px;
+    min-height: 100vw;
+    max-width: 100vw;
+`;
+
+const App = () => {
     return (
-      <Header/>
+        <GlobalStyle>
+            <Header/>
+        </GlobalStyle>
     )
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,54 @@
+import styled from "styled-components";
+import punkLogo from "../assets/images/header/punkLogo.png";
+import searchIcon from "../assets/images/header/search-icon.png";
+
+const HeaderBlock = styled.div`
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+// const LogoContainerBlock = styled.div`
+    
+// `;
+
+const PunkLogoBlock = styled.img`
+    width: 100px;
+    margin-left: 10px;
+    margin-right: 40px;
+`;
+
+const SearchBarBlock = styled.div`
+    margin: 10px;
+    display: flex;
+    flex: 1;
+    background-color: #1c1c1e;
+`;
+
+const SearchIconBlock = styled.img`
+    width: 20px;
+    margin-right: 10px;
+`;
+const SearchInputBlock = styled.input`
+    background-color: #1c1c1e;
+    border: none;
+`;
+
 const Header = () =>{
-    return(
-        <div>aa</div>
-    )
+    return (
+        <HeaderBlock>
+            <div className="logoContainer">
+                <PunkLogoBlock src={punkLogo} alt="punk-logo" />
+            </div>
+            <SearchBarBlock>
+                <div className="searchIconContainer">
+                    <SearchIconBlock src={searchIcon}/>
+                </div>
+                <SearchInputBlock placeholder="Collection, item or user."/>
+            </SearchBarBlock>
+        </HeaderBlock>
+    );
 }
 
 export default Header;
